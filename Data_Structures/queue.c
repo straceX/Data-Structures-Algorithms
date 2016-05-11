b7e1c64c30bc49742b9b95406935845dd2eca7ee
straceX <netsecdeveloper@gmail.com>
2016-05-11 16:10:49 +0300

    queue data structure function template and new file added

---
+++ Data_Structures/queue.c
@@ -0,0 +1,26 @@
+int enqueue(int ptrQueue[],int val)
+{
+
+}
+
+int dequeue(int ptrQueue[])
+{
+	
+}
+
+
+int peek(int ptrQueue[])
+{
+
+}
+
+int isfull(int ptrQueue[])
+{
+
+}
+
+int isempty(int ptrQueue[]) 
+{
+
+
+}
---
+++ Data_Structures/queue.h
@@ -0,0 +1,5 @@
+int enqueue(int *,int);
+int dequeue(int *);
+int peek(int *);
+int isfull(int *);
+int isempty(int *);
