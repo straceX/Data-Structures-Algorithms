8059617cec77b1beec4528413f742e64239f9313
0x80 <netsecdeveloper@gmail.com>
2020-04-09 15:51:52 +0200

    second version of Graph implementation is added
    In this version I used 2d array matrix

--- Data_Structures/Cplusplus/Graph/graph.cpp
+++ Data_Structures/Cplusplus/Graph/graph.cpp
@@ -1,27 +1,58 @@
 #include "graph.h"
 
-Graph::Graph(size_t vertexCount)
+Graphv1::Graph::Graph(size_t vertexCount)
 	: m_vertexCount{ vertexCount }
 	{
 	m_adjacentNodes = new list<size_t>[m_vertexCount];
 	}
 
-auto Graph::addEdge(size_t src, size_t dest)
+auto Graphv1::Graph::addEdge(size_t src, size_t dest)
 	-> void
    {
    m_adjacentNodes[src].push_back(dest);
    }
 
-auto Graph::deleteEdge(size_t src, size_t dest)
+auto Graphv1::Graph::deleteEdge(size_t src, size_t dest)
 	-> void
 	{
 	m_adjacentNodes[src].remove(dest);
 	}
 
-auto Graph::printAllAdjacentVertices()
+auto Graphv1::Graph::printAllAdjacentVertices()
    -> void
 	{
 	for (auto index = size_t{ 0 }; index < m_vertexCount; ++index)
 		for (auto edge : m_adjacentNodes[index])
       	std::cout << '[' << index << "]\t----------\t[" << edge << "]\n";
+   }
+
+Graphv2::Graph::Graph(int vertexCount)
+    : m_vertexCount{ vertexCount }
+    {
+    m_adjacentNodes = new bool*[vertexCount];
+    for (auto index = size_t{ 0 }; index < vertexCount; ++index)
+        m_adjacentNodes[index] = new bool[vertexCount]{false};
+    }
+
+auto Graphv2::Graph::addEdge(size_t src, size_t dest)
+	-> void
+   {
+   m_adjacentNodes[src][dest] = true;
+   }
+
+auto Graphv2::Graph::deleteEdge(size_t src, size_t dest)
+   -> void
+   {
+   m_adjacentNodes[src][dest] = false;
+   }
+
+auto Graphv2::Graph::printAllAdjacentVertices()
+   -> void
+   {
+   for (auto x = size_t{ 0 }; x < m_vertexCount; ++x)
+   	{
+      for (auto y = size_t{ 0 }; y < m_vertexCount; ++y)
+      	if (m_adjacentNodes[x][y])
+         	std::cout << "[" << x << "]\t----------\t[" << y << "]\n";
+      }
    }
--- Data_Structures/Cplusplus/Graph/graph.h
+++ Data_Structures/Cplusplus/Graph/graph.h
@@ -1,13 +1,33 @@
-class Graph
+namespace Graphv1
     {
-    public:
-        Graph(size_t vertexCount);
+    class Graph
+        {
+        public:
+            Graph(size_t vertexCount);
     
-        auto addEdge(size_t src, size_t dest)	 	-> void;
-        auto deleteEdge(size_t src, size_t dest)	-> void;
-        auto printAllAdjacentVertices()			-> void;
+            auto addEdge(size_t src, size_t dest)   -> void;
+            auto deleteEdge(size_t src, size_t dest)-> void;
+            auto printAllAdjacentVertices()         -> void;
     
-    private:
-        const size_t  m_vertexCount;
-        list<size_t> *m_adjacentNodes;
-    };
+        private:
+            const size_t  m_vertexCount;
+            std::list<size_t> *m_adjacentNodes;
+        };
+    }
+
+namespace Graphv2
+    {
+    class Graph
+        {
+        public:
+            Graph(size_t vertexCount);
+    
+            auto addEdge(size_t src, size_t dest)   -> void;
+            auto deleteEdge(size_t src, size_t dest)-> void;
+            auto printAllAdjacentVertices()         -> void;
+    
+        private:
+            const size_t  m_vertexCount;
+            bool        **m_adjacentNodes;
+        };
+    }
