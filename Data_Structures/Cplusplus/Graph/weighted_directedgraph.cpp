280ab85eb441d4e7a00705599d633bfc50851059
0x80 <netsecdeveloper@gmail.com>
2020-04-13 16:20:32 +0200

    Second version of Weighted Directed Graph implementation is added.
    In this version I used 2d array

--- Data_Structures/Cplusplus/Graph/weighted_directedgraph.cpp
+++ Data_Structures/Cplusplus/Graph/weighted_directedgraph.cpp
@@ -1,28 +1,60 @@
 #include "weighted_directedgraph.h"
 
-WeightedDirectedGraph::WeightedDirectedGraph(size_t vertexCount)
+WeightedDirectedGraphv1::WeightedDirectedGraph::WeightedDirectedGraph(size_t vertexCount)
 	: m_vertexCount{ vertexCount }
 	{
 	m_adjacentNodes = new list<std::pair<size_t, size_t>>[m_vertexCount];
 	}
 
-auto WeightedDirectedGraph::addEdge(size_t src, size_t dest, size_t weight)
+auto WeightedDirectedGraphv1::WeightedDirectedGraph::addEdge(size_t src, size_t dest, size_t weight)
 	-> void
 	{
 	m_adjacentNodes[src].emplace_back(dest, weight);
 	}
 
-auto WeightedDirectedGraph::deleteEdge(size_t src, size_t dest, size_t weight)
+auto WeightedDirectedGraphv1::WeightedDirectedGraph::deleteEdge(size_t src, size_t dest, size_t weight)
 	-> void
 	{
 	m_adjacentNodes[src].remove(std::make_pair{dest, weight});
 	//m_adjacentNodes[src].remove_if([](const auto rhs){ return rhs.first == dest; });
 	}
 
-auto WeightedDirectedGraph::printAllAdjacentVertices()
+auto WeightedDirectedGraphv1::WeightedDirectedGraph::printAllAdjacentVertices()
    -> void
 	{
 	for (auto index = size_t{ 0 }; index < m_vertexCount; ++index)
 		for (auto [dest, weight] : m_adjacentNodes[index])
 			std::cout << '[' << index << "]\t-----(" << weight << ")---->\t[" << dest << "]\n";
    }
+
+WeightedDirectedGraphv2::WeightedDirectedGraph::WeightedDirectedGraph(size_t vertexCount)
+    : m_vertexCount{ vertexCount }
+    {
+    m_adjacentNodes = new size_t*[vertexCount];
+    for (auto index = size_t{ 0 }; index < vertexCount; ++index)
+        m_adjacentNodes[index] = new size_t[vertexCount]{0};
+    }
+
+auto WeightedDirectedGraphv2::WeightedDirectedGraph::addEdge(size_t src, size_t dest, size_t weight)
+	-> void
+   {
+   m_adjacentNodes[src][dest] = weight;
+   }
+
+auto WeightedDirectedGraphv2::WeightedDirectedGraph::deleteEdge(size_t src, size_t dest)
+   -> void
+   {
+   m_adjacentNodes[src][dest] = 0;
+   }
+
+auto WeightedDirectedGraphv2::WeightedDirectedGraph::printAllAdjacentVertices()
+   -> void
+   {
+   for (auto x = size_t{ 0 }; x < m_vertexCount; ++x)
+   	{
+      for (auto y = size_t{ 0 }; y < m_vertexCount; ++y)
+      	if (m_adjacentNodes[x][y])
+				std::cout << '[' << index << "]\t-----(" << m_adjacentNodes[x][y] << ")---->\t[" << dest << "]\n";
+      }
+   }
+
--- Data_Structures/Cplusplus/Graph/weighted_directedgraph.h
+++ Data_Structures/Cplusplus/Graph/weighted_directedgraph.h
@@ -1,16 +1,36 @@
 #include <list>
 #include <utility>
 
-class WeightedDirectedGraph
+namespace WeightedDirectedGraphv1
     {
-    public:
-        WeightedDirectedGraph(size_t vertexCount);
+    class WeightedDirectedGraph
+        {
+        public:
+            WeightedDirectedGraph(size_t vertexCount);
     
-        auto addEdge(size_t src, size_t dest)	  -> void;
-        auto deleteEdge(size_t src, size_t dest) -> void;
-        auto printAllAdjacentVertices()          -> void;
+            auto addEdge(size_t src, size_t dest)   -> void;
+            auto deleteEdge(size_t src, size_t dest)-> void;
+            auto printAllAdjacentVertices()         -> void;
     
-    private:
-        const size_t                         m_vertexCount;
-        std::list<std::pair<size_t,size_t>> *m_adjacentNodes;
-    };
+        private:
+            const size_t                         m_vertexCount;
+            std::list<std::pair<size_t,size_t>> *m_adjacentNodes;
+        };
+    }
+
+namespace WeightedDirectedGraphv2
+    {
+    class WeightedDirectedGraph
+        {
+        public:
+            WeightedDirectedGraph(size_t vertexCount);
+    
+            auto addEdge(size_t src, size_t dest)   -> void;
+            auto deleteEdge(size_t src, size_t dest)-> void;
+            auto printAllAdjacentVertices()         -> void;
+    
+        private:
+            const size_t  m_vertexCount;
+            size_t      **m_adjacentNodes;
+        };
+    }
